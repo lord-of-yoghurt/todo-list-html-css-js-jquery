4040dc7316a7981f49ac3d2aac74c4a5794a14a2
David Ordovskiy <david.ordovsky@gmail.com>
2017-06-13 10:25:33 -0400

    add functionality to add/delete tasks

--- assets/js/lib/main.js
+++ assets/js/lib/main.js
@@ -1,4 +1,23 @@
 // mark todos complete by clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
   $(this).toggleClass("complete");
+});
+
+// click X to delete a task
+$("ul").on("click", "span", function(e) {
+  $(this).parent().fadeOut(200, function(){
+    $(this).remove();
+  });
+  e.stopPropagation();
+});
+
+// hit Enter while in the input to add task
+$("input[type='text']").keypress(function(e) {
+  if (e.which === 13) {
+    // grab user input
+    let task = $(this).val();
+    // create new li and append to ul
+    $("ul.list").append(`<li><span>X</span> ${task}</li>`);
+    $(this).val("");
+  }
 });
